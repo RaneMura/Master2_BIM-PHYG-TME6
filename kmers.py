b18007bca13b9eee0c2e04b9e69acdcb67a4c8bd
Sharane K.Murali <74711674+RaneMura@users.noreply.github.com>
2023-11-30 01:06:05 +0100

    Update kmers.py

--- kmers.py
+++ kmers.py
@@ -16,5 +16,47 @@ def kmer2str(val, k):
 
 
 def stream_kmers(text, k):
-    # --- To complete ---
-    pass
+    mask = (1 << (2*k)) - 1
+
+    kmer1 = 0
+    kmer2 = 0
+    encodage = {'A' : 0, 'C' : 1, 'T' : 2, 'G' : 3}
+    lst=[]
+
+	#Parcours pour la seq [0:k-1]    
+    for letter in text[:k-1]:
+        nucl = encodage[letter]
+        comp=(nucl+2)&0b11
+        
+        kmer1 <<= 2
+        kmer1+=nucl
+        kmer1&=mask
+
+        kmer2 <<= 2
+        kmer2+=comp
+        kmer2&=mask
+
+	#Parcours pour la seq [k-1:] décalage à jour
+    for letter in text[k-1:]:
+        
+        nucl = encodage[letter]
+        comp=(nucl+2)&0b11
+        comp <<= (2*k-2)
+        
+        kmer1 <<= 2
+        kmer1+=nucl
+        
+        kmer2 >>= 2
+        kmer2+=comp
+        
+        kmer1&=mask
+        kmer2&=mask
+
+        yield min(kmer1,kmer2)
+        
+#l = stream_kmers("AATCTGT",3)
+#for i in l :
+#	print(kmer2str(i,3))    
+
+
+
