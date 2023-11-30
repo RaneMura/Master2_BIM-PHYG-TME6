1604c0d5374740cf27148f58613767cbd4b277bb
Sharane K.Murali <74711674+RaneMura@users.noreply.github.com>
2023-11-30 11:08:08 +0100

    Update main.py
    update dataframe to csv output

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from loading import load_directory
 from kmers import stream_kmers, kmer2str
+import numpy as np
+import pandas as pd
 
 def jaccard(fileA, fileB, k):
 
@@ -7,14 +9,13 @@ def jaccard(fileA, fileB, k):
     Taille_U = 0
     Taille_I = 0
 
-    for kmer in stream_kmers(fileA[0],k):
+    for kmer in stream_kmers(fileA,k):
         dico[kmer] = 1 if kmer not in dico else dico[kmer] + 1
         Taille_U +=1
 
-    for kmer in stream_kmers(fileB[0],k):
+    for kmer in stream_kmers(fileB,k):
         
         if kmer in dico :
-            
 
             Taille_I += 1
             dico[kmer]-=1
@@ -34,7 +35,17 @@ if __name__ == "__main__":
 
     filenames = list(files.keys())
     
+    jac = np.ones((5,5))
+
     for i in range(len(files)):
         for j in range(i+1, len(files)):
-            jac = jaccard(files[filenames[i]], files[filenames[j]], k)
+        
+            jac_val = jaccard(files[filenames[i]], files[filenames[j]], k)
+            jac[i,j]  = jac_val
+            jac[j,i] = jac_val
+
             print(filenames[i], filenames[j], jac)
+
+    jac_df = pd.DataFrame(jac, index = filenames, columns = filenames)
+    jac_df.to_csv("jaccard.csv")
+
