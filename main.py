d91b726468dacfdb4868002ec8d693d5e75673a5
Sharane K.Murali <74711674+RaneMura@users.noreply.github.com>
2023-11-30 01:07:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 from loading import load_directory
 from kmers import stream_kmers, kmer2str
 
-
-
 def jaccard(fileA, fileB, k):
-    j = 0
-    # --- To complete ---
-    return j
 
+    dico = {}
+    Taille_U = 0
+    Taille_I = 0
+
+    for kmer in stream_kmers(fileA[0],k):
+        dico[kmer] = 1 if kmer not in dico else dico[kmer] + 1
+        Taille_U +=1
+
+    for kmer in stream_kmers(fileB[0],k):
+        
+        if kmer in dico :
+            
+
+            Taille_I += 1
+            dico[kmer]-=1
+            if dico[kmer] == 0:
+                del dico[kmer]
+		
+        else:
+            Taille_U += 1
+          
+    return Taille_I/Taille_U
 
 
 if __name__ == "__main__":
     # Load all the files in a dictionary
     files = load_directory("data")
     k = 21
-    
+
     filenames = list(files.keys())
+    
     for i in range(len(files)):
         for j in range(i+1, len(files)):
-            
-            # --- Complete here ---
-
-            j = jaccard(files[filenames[i]], files[filenames[j]], k)
-            print(filenames[i], filenames[j], j)
+            jac = jaccard(files[filenames[i]], files[filenames[j]], k)
+            print(filenames[i], filenames[j], jac)
