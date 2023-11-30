8f5a9fa0d0f5b5b047d872e58aa8a1202fbf8564
Sharane K.Murali <74711674+RaneMura@users.noreply.github.com>
2023-11-30 01:06:44 +0100

    Update loading.py

--- loading.py
+++ loading.py
@@ -1,5 +1,5 @@
 from os import listdir, path
-
+import re
 
 def load_fna(filename):
     """ Loads a fasta formated file into a list of sequences.
@@ -16,12 +16,12 @@ def load_fna(filename):
                     texts.append("".join(txt))
                 txt = []
             else:
-                txt.append(line.strip())
+                line_wo = re.sub(r'[^ACTG]','',line.strip())
+                txt.append(line_wo)
 
     if len(txt) > 0:
         texts.append("".join(txt))
     return texts
-
 
 
 def load_directory(directory):
